15209a36791669a15499cd05ee9572111ae980d6
jiyuanshan <jiyuanshan1@gmail.com>
2025-10-22 15:50:17 +0200

    update video

--- get_TCP.py
+++ get_TCP.py
@@ -1,3 +1,3 @@
 from rtde_receive import RTDEReceiveInterface
-rtde_r = RTDEReceiveInterface("172.17.21.11")  # 替换成你的UR5 IP
+rtde_r = RTDEReceiveInterface("172.17.21.11") 
 print(rtde_r.getActualTCPPose())
